d6da009d9af6898c2a14c438991d0acd0adbc401
Wellington Pardim Ferreira <pardim@MacBook-Pro-de-Wellington.local>
2015-02-13 15:30:59 -0200

    Classe rank

---
+++ GeniusRoarXXXDarklords/GeniusRoarXXXDarklords/Rank.h
@@ -0,0 +1,13 @@
+//
+//  Rank.h
+//  GeniusRoarXXXDarklords
+//
+//  Created by Wellington Pardim Ferreira on 2/13/15.
+//  Copyright (c) 2015 Os Sobras. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Rank : NSObject
+
+@end
