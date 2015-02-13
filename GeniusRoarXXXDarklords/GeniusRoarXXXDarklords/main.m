33b2ddaa68a80168aba15ab1910df2f9d963b18c
Vitor Kawai Sala <vitorkawai@MacBook-Pro-de-Vitor.local>
2015-02-13 15:03:58 -0200

    commit inicial

---
+++ GeniusRoarXXXDarklords/GeniusRoarXXXDarklords/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GeniusRoarXXXDarklords
+//
+//  Created by Vitor Kawai Sala on 13/02/15.
+//  Copyright (c) 2015 Os Sobras. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
