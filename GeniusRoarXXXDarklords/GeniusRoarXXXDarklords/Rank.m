96eca20fda8428a65ea4b99a1650308fda878660
Vitor Kawai Sala <vitorkawai@MacBook-Pro-de-Vitor.local>
2015-02-13 17:16:07 -0200

    Implementação de Rank

--- GeniusRoarXXXDarklords/GeniusRoarXXXDarklords/Rank.m
+++ GeniusRoarXXXDarklords/GeniusRoarXXXDarklords/Rank.m
@@ -13,36 +13,75 @@
 -(instancetype)init{
     self = [super init];
     if(self){
-
+        rank = [[NSMutableArray alloc] init];
     }
     return self;
 }
 
 -(Jogador *)selecionarUmJogador:(NSString *)nome{
+    int pos = [self pegarPosicaoDoJogadorPeloNome:nome];
+    if(pos != -1){
+        jogadorAtual = [rank objectAtIndex:pos];
+        [rank removeObjectAtIndex:pos];
+        return jogadorAtual;
+    }
     return nil;
 }
 
--(void)adicionarUmJogador:(Jogador *)jogador{
+-(void)adicionarUmJogadorExistente:(Jogador *)jogador{
+    int pontuacao = [jogador mPontos];
+    int size = (int)[rank count];
+    int i = 0;
+    while (i < size && [(Jogador *)[rank objectAtIndex:i] mPontos] > pontuacao);
+    [rank insertObject:jogador atIndex:i];
+}
 
+-(void)adicionarUmJogadorNovo:(NSString *)nome{
+    jogadorAtual = [[Jogador alloc] init];
+    [jogadorAtual setNomeUsuario:nome];
+    [jogadorAtual setQPontos:0];
+    [jogadorAtual setMPontos:0];
 }
 
 -(Jogador *)pegarPrimeiroLugar{
-    return nil;
+    return [rank firstObject];
 }
 
--(Jogador *)pegarJogadorAtual{
-    return nil;
+-(void)removerJogador:(Jogador *)jogador{
+    int pos = [self pegarPosicaoDoJogador:jogador];
+    [rank removeObjectAtIndex:pos];
 }
 
--(void)removerJogador:(NSString *)nome{
-
+-(int)pegarPosicaoDoJogador:(Jogador *)jogador{
+    int size = (int)[rank count];
+    for(int i = 0 ; i < size ; i++){
+        Jogador *j = [rank objectAtIndex:i];
+        if([[j nomeUsuario] compare:[jogador nomeUsuario]])
+            return i;
+    }
+    return -1;
 }
 
--(void)zerarListaDeJogadores{
-
+-(int)pegarPosicaoDoJogadorPeloNome:(NSString *)nome{
+    int size = (int)[rank count];
+    for(int i = 0 ; i < size ; i++){
+        Jogador *j = [rank objectAtIndex:i];
+        if([[j nomeUsuario] compare:nome])
+            return i;
+    }
+    return -1;
 }
+
+-(Fila *)pegarRankDeJogadores{
+    Fila *f = [[Fila alloc] init];
+    for(Jogador *j in rank){
+        [f enfileirar:j];
+    }
+    return f;
+}
+
 
 @synthesize jogadorAtual;
 
-
 @end
+
