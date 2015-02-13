cb95bc2d493eae23b7109404aa575655b7c08ee4
Vitor Kawai Sala <vitorkawai@MacBook-Pro-de-Vitor.local>
2015-02-13 15:14:35 -0200

    Adicionando Enumeração de cores (teste de integração do git com o xcode)

---
+++ GeniusRoarXXXDarklords/GeniusRoarXXXDarklords/Cores.h
@@ -0,0 +1,19 @@
+//
+//  Cores.h
+//  GeniusRoarXXXDarklords
+//
+//  Created by Vitor Kawai Sala on 13/02/15.
+//  Copyright (c) 2015 Os Sobras. All rights reserved.
+//
+
+#ifndef GeniusRoarXXXDarklords_Cores_h
+#define GeniusRoarXXXDarklords_Cores_h
+
+enum Cores {
+    vermelho,
+    amarelo,
+    azul,
+    verde
+};
+
+#endif
